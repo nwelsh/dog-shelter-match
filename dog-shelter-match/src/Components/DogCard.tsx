31337ad491bc3e5d7c4712b47efed4fb018b34d6
nwelsh <newelsh@wisc.edu>
2025-06-18 13:56:08 -0400

    heart icon

--- dog-shelter-match/src/Components/DogCard.tsx
+++ dog-shelter-match/src/Components/DogCard.tsx
@@ -22,33 +22,60 @@ const DogCard = React.forwardRef<HTMLDivElement, DogCardProps>(
   ({ dog, isFavorite, onToggleFavorite, isMatch }, ref) => {
     const { img, name, breed, age, zip_code, id } = dog;
 
-  return (
-    <div className={classNames('dog-card', {'dog-card--match': isMatch})} ref={ref} onClick={() => onToggleFavorite(id)}>
-    {isMatch && <p className="exit-button" onClick={() => !isMatch}>X</p>}
-    <button
+    return (
+      <div
+        className={classNames("dog-card", { "dog-card--match": isMatch })}
+        ref={ref}
+        onClick={() => onToggleFavorite(id)}
+      >
+        {isMatch && (
+          <p className="exit-button" onClick={() => !isMatch}>
+            X
+          </p>
+        )}
+        <button
           className={`favorite-btn ${isFavorite ? "favorited" : ""}`}
           onClick={() => onToggleFavorite(id)}
           aria-label={isFavorite ? "Remove from favorites" : "Add to favorites"}
         >
-          {isFavorite ? "❤️" : "🤍"}
+          <svg
+  width="20"
+  height="18.5"
+  viewBox="-2 -2 36 34"
+  fill={isFavorite ? "red" : "none"}
+  stroke="currentColor"
+  strokeWidth="3"
+  strokeLinecap="round"
+  strokeLinejoin="round"
+  className="heart-icon"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    d="M23.6,0c-3.4,0-6.4,2.2-7.6,5.3C14.8,2.2,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4
+       c0,9.4,16,21.2,16,21.2s16-11.8,16-21.2C32,3.8,28.2,0,23.6,0z"
+  />
+</svg>
         </button>
-      <img className={classNames("image", { "image--match": isMatch })} src={img} alt={`Photo of ${name}`} />
-      <div className="description-container">
-        <h3 className="name">{name}</h3>
-        <p className="description">
-          <strong>Breed:</strong> {breed}
-        </p>
-        <p className="description">
-          <strong>Age:</strong> {age}
-        </p>
-        <p className="description">
-          <strong>Zip Code:</strong> {zip_code}
-        </p>
-
-       
+        <img
+          className={classNames("image", { "image--match": isMatch })}
+          src={img}
+          alt={`Photo of ${name}`}
+        />
+        <div className="description-container">
+          <h3 className="name">{name}</h3>
+          <p className="description">
+            <strong>Breed:</strong> {breed}
+          </p>
+          <p className="description">
+            <strong>Age:</strong> {age}
+          </p>
+          <p className="description">
+            <strong>Zip Code:</strong> {zip_code}
+          </p>
+        </div>
       </div>
-    </div>
-  );
-});
+    );
+  }
+);
 
 export default DogCard;
